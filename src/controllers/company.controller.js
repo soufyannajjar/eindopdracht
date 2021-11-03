c8d70b7721c8b0ea60b3428cb0abacffb38779e3
soufyannajjar <soufyan.najjar@student.ehb.be>
2021-11-03 16:16:28 +0100

    Add method documentation

--- src/controllers/company.controller.js
+++ src/controllers/company.controller.js
@@ -1,13 +1,26 @@
-
+/**
+ * Get all companies
+ * @param {*} req 
+ * @param {*} res 
+ */
 exports.all = async(req, res) => {
     res.status(200).send({ "Company":"ALL" })
 }
 
-
+/**
+ * Update a video
+ * @param {*} req 
+ * @param {*} res 
+ */
 exports.update = async(req, res) => {
     res.status(200).send({ "Company":"Update" })
 }
 
+/**
+ * Delete a video
+ * @param {*} req 
+ * @param {*} res 
+ */
 exports.delete = async(req, res) => {
     res.status(200).send({ "Company":"Delete" })
 }
