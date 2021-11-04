8d51c97db4fe5a6b6cee2d6cd8beccbba35a8600
soufyannajjar <soufyan.najjar@student.ehb.be>
2021-11-04 10:41:17 +0100

    Endpoint '/companies' Get companies & their videos

--- src/controllers/company.js
+++ src/controllers/company.js
@@ -1,29 +1,37 @@
-const company = require("./../models/company");
+const company = require('./../models/company')
 /**
  * Get all companies
- * @param {*} req 
- * @param {*} res 
+ * @param {*} req
+ * @param {*} res
  */
-exports.all = async(req, res) => {
-    company.findAll()
-        .then(res => res.send(res))
-        .catch(err => res.send(err))
+exports.all = async (req, res) => {
+  company
+    .findAll()
+    .then((companies) => {
+      return res.status(200).send({
+        count: companies.length,
+        companies,
+      })
+    })
+    .catch((err) => {
+      return res.status(500).send(err)
+    })
 }
 
 /**
  * Update a video
- * @param {*} req 
- * @param {*} res 
+ * @param {*} req
+ * @param {*} res
  */
-exports.update = async(req, res) => {
-    res.status(200).send({ "Company":"Update" })
+exports.update = async (req, res) => {
+  res.status(200).send({ Company: 'Update' })
 }
 
 /**
  * Delete a video
- * @param {*} req 
- * @param {*} res 
+ * @param {*} req
+ * @param {*} res
  */
-exports.delete = async(req, res) => {
-    res.status(200).send({ "Company":"Delete" })
+exports.delete = async (req, res) => {
+  res.status(200).send({ Company: 'Delete' })
 }
--- src/models/company.js
+++ src/models/company.js
@@ -1,8 +1,40 @@
 const db = require('../helpers/DatabaseHelper');
-const { T_COMPANY } = require('../utils/Constants');
+const { T_COMPANY, T_VIDEO } = require('../utils/Constants');
+const joinjs = require('join-js').default;
+
+const resultMaps = [
+    {
+        mapId: 'companyMap',
+        idProperty: 'id',
+        properties: ['name','email'],
+        collections: [
+            {name: 'videos', mapId: 'videoMap', columnPrefix: 'video_'}
+        ]
+    },
+    {
+        mapId: 'videoMap',
+        idProperty: 'id',
+        properties: ['title','path']
+    }
+]
+
+const findAll = () => {
+    return db
+    .select( 'co.id as company_id',
+    'co.name as company_name',
+    'co.email as company_email',
+    'vi.id as video_id',
+    'vi.title as video_title',
+    'vi.path as video_path')
+    .from(`${T_COMPANY} as co`)
+    .leftOuterJoin(`${T_VIDEO} as vi`, 'vi.id_company','co.id')
+    .orderBy("co.id","asc")
+    .then((resultSet) => {
+        return joinjs.map(resultSet, resultMaps, 'companyMap', 'company_');
+    });
+}
 
 
-const findAll = () => db(T_COMPANY).select("*");
 
 
 module.exports = {findAll};
