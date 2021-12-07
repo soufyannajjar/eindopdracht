a686342a28c2d2edf13cbb3daf79d208f716f3a2
soufyannajjar <soufyan.najjar@student.ehb.be>
2021-12-07 13:00:57 +0100

    Company route for POST | PUT | DELETE

--- api/src/routes/route.js
+++ api/src/routes/route.js
@@ -4,9 +4,13 @@ const router = express.Router();
 const company = require('../controllers/company');
 const movie = require('../controllers/movie')
 
-
+//Company
 router.get('/companies', company.all);
 router.post('/companies', company.add);
+router.put('/companies/:id', company.update);
+router.delete('/companies/:id', company.delete);
+
+
 router.put('/videos/:id', movie.update);
 router.delete('/videos/:id', movie.delete);
 router.use((req, res) => {
